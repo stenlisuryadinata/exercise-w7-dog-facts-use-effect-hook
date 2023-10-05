84a13b7bf64c1a7562e214e11a2af3fc69a9f75d
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-10-05 15:41:47 +0700

    challenge solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 // Import required  hooks
+import { useState, useEffect } from "react";
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+  const [fact, setFact] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dogapi.dog/api/v2/facts")
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeout(() => {
+          setFact(data.data[0].attributes);
+        }, 2000);
+      });
+  }, []);
   // Hint: Define the API endpoint
   // Hint: Create a function to fetch the dog fact
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
 
   return (
     <div className="App">
-      <DogFact />
+      <h1>Fun Dog Facts</h1>
+      <div className="text_box">
+        {fact.length !== 0 ? <DogFact newfacts={fact} /> : <p className="loading">Loading</p>}
+      </div>
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,9 +1,20 @@
 // DogFact Component
-export const DogFact = () => {
+
+import PropTypes from "prop-types";
+export const DogFact = ({ newfacts }) => {
+  console.log("DogComponent", newfacts);
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return (
+    <div>
+      <p>{newfacts.body}</p>
+    </div>
+  );
+};
+
+DogFact.propTypes = {
+  newfacts: PropTypes.object,
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
